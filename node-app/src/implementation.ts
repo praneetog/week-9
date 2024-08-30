58ab549965532f2278931993988bef1b26551cf9
praneet <praneetkashyap001@gmail.com>
2024-08-30 15:16:16 +0530

    new commits

---
+++ node-app/src/implementation.ts
@@ -0,0 +1,35 @@
+interface Person{
+    name: string,
+    age: number,
+    greet(phrase: string): void
+}
+
+class Employee implements Person{
+    name: string;
+    age: number;
+
+    constructor(n: string, a: number){
+        this.name = n;
+        this.age = a;
+    }
+    greet(phrase: string){
+        console.log(`${phrase} ${this.name}`);
+    }
+}
+
+class Manager implements Person{
+    name: string;
+    age: number;
+
+    constructor(n: string, a: number){
+        this.name = n;
+        this.age = a;
+    }
+
+    greet(phrase: string){
+        console.log(`${phrase} ${this.name}`);
+    }
+}
+
+const e = new Employee("Praneet", 20);
+console.log(e.greet("Hi there,"));
